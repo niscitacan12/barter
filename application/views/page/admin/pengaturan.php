69d0ee7fc4e040a8c25195d8b4c04ea8e632b56a
azifaa <dwiazifatuljannah@gmail.com>
2023-11-10 13:49:22 +0700

    ProfilePengaturan

--- application/views/page/admin/pengaturan.php
+++ application/views/page/admin/pengaturan.php
@@ -21,80 +21,59 @@
                 <h6 class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Pengaturan Web</h6>
             </div>
             
-            
-            <div class="inline-flex rounded-md shadow-sm" role="group">
-            <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                Pengaturan Web
-            </button>
-            <button type="button" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
-                Profile
-            </button>
-            </div>
             <div class="mt-5 text-left">
-
+                
+            <div class="inline-flex rounded-md shadow-sm" role="group">
+                <button type="button" onclick="window.location.href = '<?php echo base_url('admin/pengaturan'); ?>'" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-l-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                    Pengaturan Web
+                </button>
+                <button type="button" onclick="window.location.href = '<?php echo base_url('admin/profile_pengaturan'); ?>'" class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-r-md hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white">
+                    Profile
+                </button>
+            </div>
 
                     <!-- Form Input -->
-                    <form action="<?php echo base_url('admin/aksi_tambah_user') ?>" method="post"
+                    <form action="#" method="post"
                         enctype="multipart/form-data">
-                        
-                        <!-- Email Input -->
                        
                         <!-- Nama Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="text" name="nama" id="nama"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for="nama"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama</label>
-                        </div>
+                        <div>
+                        <label for="nama" class="block text-gray-700 font-bold mb-2">Nama:</label>
+                        <input type="text" id="nama" name="nama" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500" required>
+                    </div>
                         <!-- Deskripsi Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="text" name="deskripsi" id="deskripsi"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for="deskripsi"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Deskripsi</label>
+                        <div>
+                            <label for="deskripsi" class="block text-gray-700 font-bold mb-2">Deskripsi:</label>
+                            <input type="text" id="deskripsi" name="deskripsi" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500" required>
                         </div>
                         <!-- No Telepon Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="text" name="no telepon" id="no telepon"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for="no telepon"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">No Telepon</label>
+                        <div>
+                            <label for="no telepon" class="block text-gray-700 font-bold mb-2">No Telepon:</label>
+                            <input type="no" id="no telepon" name="no telepon" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500" required>
                         </div>
                         <!-- Alamat Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="text" name=" Alamat" id=" Alamat"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for=" Alamat"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Alamat</label>
+                        <div>
+                            <label for=" alamat" class="block text-gray-700 font-bold mb-2"> Alamat:</label>
+                            <input type="text" id=" alamat" name=" alamat" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500"  required>
                         </div>
                         <!--  Email Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="email" name=" email" id=" email"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for=" email"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Email</label>
+                        <div>
+                            <label for=" email" class="block text-gray-700 font-bold mb-2">Email:</label>
+                            <input type="email" id=" email" name=" email" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500" required>
                         </div>
                         <!--  Email domain Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="email" name=" email domain" id=" email domain"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for=" email domain"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Email Domain</label>
+                       <div>
+                            <label for=" email domain" class="block text-gray-700 font-bold mb-2"> Email Domain:</label>
+                            <input type="email" id=" email domain" name=" email domain" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500" required>
                         </div>
                         <!--  Alamat Website Input -->
-                        <div class="relative z-0 w-full mb-6 group">
-                            <input type="text" name=" alamat website" id=" alamat website"
-                                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" required />
-                            <label for=" alamat website"
-                                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6"> Alamat Website</label>
+                        <div>
+                            <label for=" alamat Website" class="block text-gray-700 font-bold mb-2"> alamat Website:</label>
+                            <input type="text" id=" alamat Website" name=" alamat Website" class="w-full border-2 border-gray-300 p-2 rounded-md focus:outline-none focus:border-indigo-500" required>
                         </div>
+                        <br>
+                        <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your message</label>
+                     <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
                     </form>
 
                 </div>

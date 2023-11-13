c5322e4b9647c1bc02480b38813b4d7ff1a5ef23
niscita <citacandrarini@gmail.com>
2023-11-13 16:20:08 +0700

    pageDashboard

--- application/views/page/admin/dashboard.php
+++ application/views/page/admin/dashboard.php
@@ -11,7 +11,47 @@
     <?php $this->load->view('components/sidebar_admin'); ?>
     <div class="p-4 sm:ml-64">
         <div class="p-5 mt-10">
-            <h1>ini Dashboard</h1>
+            <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+                <a href="<?= ('admin/user') ?>"
+                    class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                    <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">User</h5>
+                    <hr class="mb-4">
+                    <div class="flex justify-between">
+                        <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+                            <!-- <?= $user ?> User -->
+                        </p>
+                        <div>
+                            <i class="fa-solid fa-users-gear fa-2xl"></i>
+                        </div>
+                    </div>
+                </a>
+                <a href="<?= ('admin/absensi') ?>"
+                class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Absensi</h5>
+                <hr class="mb-4">
+                <div class="flex justify-between">
+                    <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+                        <!-- <?= $absensi ?> Absensi -->
+                    </p>
+                    <div>
+                        <i class="fa-solid fa-address-card fa-2xl"></i>
+                    </div>
+                </div>
+            </a>
+            <a href="<?= ('admin/cuti') ?>"
+                class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                <h5 class="mb-2 text-3xl font-bold text-gray-900 dark:text-white">Cuti</h5>
+                <hr class="mb-4">
+                <div class="flex justify-between">
+                    <p class="mb-5 text-base text-gray-500 sm:text-lg dark:text-gray-400">
+                        <!-- <?= $cuti ?> Cuti -->
+                    </p>
+                    <div>
+                        <i class="fa-solid fa-calendar-alt fa-fw fa-2xl"></i>
+                    </div>
+                </div>
+            </a>
+            </div>
         </div>
     </div>
 </body>
--- application/views/page/super_admin/organisasi.php
+++ application/views/page/super_admin/organisasi.php
@@ -82,8 +82,8 @@
                                 <td class="px-6 py-4">
                                     <div class="flex justify-content-between">
                                         <a type="button" href="<?= base_url(
-                                            'superadmin/detail_organisasi'
-                                        ) ?>"
+                                            'superadmin/detail_organisasi?id=' . 
+                                                $row->id_organisasi) ?>"
                                             class="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 mx-1 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
                                             <i class="fa-solid fa-circle-info"></i>
                                         </a>

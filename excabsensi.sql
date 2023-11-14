023c019662896903855e557ed7850fc61839dbca
Aveceena07 <mingave11@gmail.com>
2023-11-14 09:14:03 +0700

    memperbarui database

--- excabsensi.sql
+++ excabsensi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 06, 2023 at 03:26 AM
+-- Generation Time: Nov 14, 2023 at 03:02 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -39,6 +39,73 @@ CREATE TABLE `absensi` (
   `lokasi` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `absensi`
+--
+
+INSERT INTO `absensi` (`id_absensi`, `id_user`, `kegiatan`, `tanggal_absen`, `keterangan_izin`, `jam_masuk`, `jam_pulang`, `status`, `lokasi`) VALUES
+(1, 14, 'Nyapu', '2023-11-10', '', '17:00:00', '18:00:00', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `nama_depan` varchar(255) NOT NULL,
+  `nama_belakang` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `id_superadmin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `email`, `username`, `nama_depan`, `nama_belakang`, `password`, `image`, `role`, `id_superadmin`) VALUES
+(1, 'dani@gmail.com', 'Dani', 'Affandi', 'Khoir', '25d55ad283aa400af464c76d713c07ad', 'User.png', 'admin', 1),
+(2, 'ida@gmail.com', 'ida', 'ida', 'ida', '25d55ad283aa400af464c76d713c07ad', 'User.png', 'admin', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jabatan`
+--
+
+CREATE TABLE `jabatan` (
+  `id_jabatan` int(11) NOT NULL,
+  `nama_jabatan` varchar(100) NOT NULL,
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jabatan`
+--
+
+INSERT INTO `jabatan` (`id_jabatan`, `nama_jabatan`, `id_admin`) VALUES
+(1, 'Kepsek', 1),
+(2, 'Pak Bon', 2),
+(4, 'Murid', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `lokasi`
+--
+
+CREATE TABLE `lokasi` (
+  `id_lokasi` int(11) NOT NULL,
+  `nama_lokasi` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 -- --------------------------------------------------------
 
 --
@@ -53,18 +120,66 @@ CREATE TABLE `organisasi` (
   `email_organisasi` varchar(255) NOT NULL,
   `kecamatan` varchar(100) NOT NULL,
   `kabupaten` varchar(100) NOT NULL,
-  `provinsi` varchar(100) NOT NULL
+  `provinsi` varchar(100) NOT NULL,
+  `id_admin` int(11) DEFAULT NULL,
+  `id_superadmin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `organisasi`
 --
 
-INSERT INTO `organisasi` (`id_organisasi`, `nama_organisasi`, `alamat`, `nomor_telepon`, `email_organisasi`, `kecamatan`, `kabupaten`, `provinsi`) VALUES
-(1, 'smk', 'jl', '0987543678', 'smk@gmail.com', 'bergas', 'semarang', 'jawa tengah'),
-(2, 'jhguifdg', 'dsgdgfh', '12344', 'a@gmail.com', 'dsgdhg', 'fdhgfh', 'gfhgfdh'),
-(3, 'Excelent', 'Salatiga', '08974857454', 'exc@gmail.com', 'Salatiga', 'Semarang', 'Jateng'),
-(4, 'MTs H M Subandi', 'Jl.kyai gandek Gandekan', '0812345678', 'subandi@gmail.com', 'Bawen', 'Semarang', 'Jawa Tengah');
+INSERT INTO `organisasi` (`id_organisasi`, `nama_organisasi`, `alamat`, `nomor_telepon`, `email_organisasi`, `kecamatan`, `kabupaten`, `provinsi`, `id_admin`, `id_superadmin`) VALUES
+(7, 'dwdw', 'dfgdg', '454545', 'binus@gmail.com', 'fgdfg', 'dfgdfg', 'dfgdg', 1, 0),
+(8, 'MTs H M Subandi ', 'Gandekan', '09765544546', 'subandi@gmail.com', 'Bawen', 'Semarang', 'Jawa', NULL, 0),
+(9, 'Negri 10000 Bawen', 'Bawen', '123', 'bawen@gmail.com', 'Bawen', 'Semarang', 'Jateng', NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shift`
+--
+
+CREATE TABLE `shift` (
+  `id_shift` int(11) NOT NULL,
+  `nama_shift` varchar(255) NOT NULL,
+  `jam_masuk` time NOT NULL,
+  `jam_pulang` time NOT NULL,
+  `id_admin` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `shift`
+--
+
+INSERT INTO `shift` (`id_shift`, `nama_shift`, `jam_masuk`, `jam_pulang`, `id_admin`) VALUES
+(1, 'Normal', '07:00:00', '14:00:00', 1),
+(2, 'Malam', '19:00:00', '02:00:00', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superadmin`
+--
+
+CREATE TABLE `superadmin` (
+  `id_superadmin` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `nama_depan` varchar(255) NOT NULL,
+  `nama_belakang` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `superadmin`
+--
+
+INSERT INTO `superadmin` (`id_superadmin`, `email`, `username`, `nama_depan`, `nama_belakang`, `password`, `image`, `role`) VALUES
+(1, 'dewi@gmail.com', 'dewi', 'dewi', 'pulung', '25d55ad283aa400af464c76d713c07ad', 'User.png', 'superadmin'),
+(2, 'ave@gmail.com', 'Ave', 'Ave', 'Ceena', '25d55ad283aa400af464c76d713c07ad', 'User.png', 'superadmin');
 
 -- --------------------------------------------------------
 
@@ -73,29 +188,31 @@ INSERT INTO `organisasi` (`id_organisasi`, `nama_organisasi`, `alamat`, `nomor_t
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `username` varchar(100) NOT NULL,
-  `nama_depan` varchar(100) NOT NULL,
-  `nama_belakang` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `nama_depan` varchar(255) NOT NULL,
+  `nama_belakang` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `image` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
   `id_admin` int(11) NOT NULL,
-  `id_superadmin` int(11) NOT NULL,
   `id_organisasi` int(11) NOT NULL,
-  `role` varchar(10) NOT NULL
+  `id_jabatan` int(11) NOT NULL,
+  `id_shift` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `email`, `username`, `nama_depan`, `nama_belakang`, `password`, `image`, `id_admin`, `id_superadmin`, `id_organisasi`, `role`) VALUES
-(1, 'dewi@gmail.com', 'dewi', 'dewi', 'pulung', '25d55ad283aa400af464c76d713c07ad', '', 0, 0, 0, 'superadmin'),
-(2, 'aveceena@gmail.com', 'ave', 'aveceena', 'ming', '25d55ad283aa400af464c76d713c07ad', '', 0, 1, 1, 'admin'),
-(3, 'dhani@gmail.com', 'dhani', 'dhani', 'dhan', '25d55ad283aa400af464c76d713c07ad', '', 2, 1, 1, 'user'),
-(4, 'niki@gmail.com', 'niki12', 'niki', 'kirana', '25d55ad283aa400af464c76d713c07ad', 'User.png', 0, 0, 0, 'user'),
-(9, 'aruna@gmail.com', 'runaa', 'aruna', 'maida', '25d55ad283aa400af464c76d713c07ad', 'User.png', 0, 1, 3, 'admin');
+INSERT INTO `user` (`id_user`, `email`, `username`, `nama_depan`, `nama_belakang`, `password`, `image`, `role`, `id_admin`, `id_organisasi`, `id_jabatan`, `id_shift`) VALUES
+(14, 'fajar@gmail.com', 'efefe', 'yfytf', 'tfytfyfy', '25d55ad283aa400af464c76d713c07ad', '', 'user', 1, 7, 1, 1),
+(16, 'sari@gmail.com', 'dewi', 'dewi', 'sari', '25d55ad283aa400af464c76d713c07ad', '', 'user', 2, 7, 1, 1),
+(17, 'ave@gmail.com', 'ave', 'ceena', 'ave', '25d55ad283aa400af464c76d713c07ad', '', 'user', 2, 7, 2, 1),
+(21, 'ceena@gmail.com', 'Ceena', 'Ave', 'Ceena', 'ifuhruhfruhf', '', 'user', 1, 8, 2, 1),
+(32, 'ffdfd@gmail.com', 'aa', 'aa', 'aa', '9dcbf642c78137f656ba7c24381ac25b', 'User.png', 'user', 2, 9, 4, 2),
+(33, 'mala@gmail.com', 'Mala', 'Mala', 'Nada', '25d55ad283aa400af464c76d713c07ad', 'User.png', 'user', 1, 8, 2, 1);
 
 --
 -- Indexes for dumped tables
@@ -106,21 +223,58 @@ INSERT INTO `user` (`id`, `email`, `username`, `nama_depan`, `nama_belakang`, `p
 --
 ALTER TABLE `absensi`
   ADD PRIMARY KEY (`id_absensi`),
-  ADD KEY `absensi_ibfk_1` (`id_user`);
+  ADD KEY `fk_user_id` (`id_user`);
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`),
+  ADD KEY `fk_superadmin_id` (`id_superadmin`);
+
+--
+-- Indexes for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD PRIMARY KEY (`id_jabatan`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD PRIMARY KEY (`id_lokasi`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `organisasi`
 --
 ALTER TABLE `organisasi`
-  ADD PRIMARY KEY (`id_organisasi`);
+  ADD PRIMARY KEY (`id_organisasi`),
+  ADD KEY `fk_admin_id` (`id_admin`);
+
+--
+-- Indexes for table `shift`
+--
+ALTER TABLE `shift`
+  ADD PRIMARY KEY (`id_shift`),
+  ADD KEY `id_admin` (`id_admin`);
+
+--
+-- Indexes for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  ADD PRIMARY KEY (`id_superadmin`);
 
 --
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_user`),
   ADD KEY `id_admin` (`id_admin`),
-  ADD KEY `user_ibfk_1` (`id_organisasi`);
+  ADD KEY `id_organisasi` (`id_organisasi`),
+  ADD KEY `id_jabatan` (`id_jabatan`),
+  ADD KEY `id_shift` (`id_shift`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -130,19 +284,49 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `absensi`
 --
 ALTER TABLE `absensi`
-  MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_absensi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  MODIFY `id_jabatan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  MODIFY `id_lokasi` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `organisasi`
 --
 ALTER TABLE `organisasi`
-  MODIFY `id_organisasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_organisasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `shift`
+--
+ALTER TABLE `shift`
+  MODIFY `id_shift` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `superadmin`
+--
+ALTER TABLE `superadmin`
+  MODIFY `id_superadmin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Constraints for dumped tables
@@ -152,13 +336,46 @@ ALTER TABLE `user`
 -- Constraints for table `absensi`
 --
 ALTER TABLE `absensi`
-  ADD CONSTRAINT `absensi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `admin`
+--
+ALTER TABLE `admin`
+  ADD CONSTRAINT `fk_superadmin_id` FOREIGN KEY (`id_superadmin`) REFERENCES `superadmin` (`id_superadmin`);
+
+--
+-- Constraints for table `jabatan`
+--
+ALTER TABLE `jabatan`
+  ADD CONSTRAINT `jabatan_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`);
+
+--
+-- Constraints for table `lokasi`
+--
+ALTER TABLE `lokasi`
+  ADD CONSTRAINT `lokasi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `organisasi`
+--
+ALTER TABLE `organisasi`
+  ADD CONSTRAINT `fk_admin_id` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`);
+
+--
+-- Constraints for table `shift`
+--
+ALTER TABLE `shift`
+  ADD CONSTRAINT `shift_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`);
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_organisasi`) REFERENCES `organisasi` (`id_organisasi`);
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`id_admin`) REFERENCES `admin` (`id_admin`),
+  ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`id_organisasi`) REFERENCES `organisasi` (`id_organisasi`),
+  ADD CONSTRAINT `user_ibfk_3` FOREIGN KEY (`id_jabatan`) REFERENCES `jabatan` (`id_jabatan`),
+  ADD CONSTRAINT `user_ibfk_4` FOREIGN KEY (`id_shift`) REFERENCES `shift` (`id_shift`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

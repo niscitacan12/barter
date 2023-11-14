b23e52f5762ce42ea4b0617e152bcce1353fe00b
lintang31 <setiaalintang@gmail.com>
2023-11-14 13:34:04 +0700

    crud organisasi role admin

--- application/views/page/admin/tambah_organisasi.php
+++ application/views/page/admin/tambah_organisasi.php
@@ -10,101 +10,147 @@
 <body>
     <?php $this->load->view('components/sidebar_admin'); ?>
     <div class="p-4 sm:ml-64">
-        <div class="p-5 mt-5">
+        <div class="p-5 mt-10">
+
             <!-- Card -->
             <div
                 class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+
+                <!-- Header -->
                 <div class="flex justify-between">
                     <h6 class="mb-2 text-xl font-bold text-gray-900 dark:text-white">Tambah Organisasi</h6>
                 </div>
+
                 <hr>
 
                 <div class="mt-5 text-left">
-                    <form>
+
+                    <!-- Form Input -->
+                    <form action="<?php echo base_url('admin/aksi_tambah_organisasi') ?>" method="post"
+                        enctype="multipart/form-data">
+
                         <div class="grid md:grid-cols-2 md:gap-6">
-                            <div class="mb-6 w-full">
-                                <label for="alamat"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+
+                            <!-- Organisasi Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="nama_organisasi" id="nama_organisasi"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
+                                <label for="nama_organisasi"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Nama Organisasi
                                 </label>
-                                <input type="text" id="alamat"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="" autocomplete="off">
                             </div>
 
-                            <div class="mb-6 w-full">
-                                <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                    Email
-                                </label>
-                                <input type="email" id="email"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="" autocomplete="off">
-                            </div>
-                        </div>
-
-                        <div class="grid md:grid-cols-2 md:gap-6">
-                            <div class="mb-6 w-full">
+                            <!-- Alamat Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="alamat" id="alamat"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
                                 <label for="alamat"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Alamat
                                 </label>
-                                <input type="text" id="alamat"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="" autocomplete="off">
-                            </div>
-
-                            <div class="mb-6 w-full">
-                                <label for="alamat"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                    No Telp
-                                </label>
-                                <input type="text" id="alamat"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="" autocomplete="off">
                             </div>
                         </div>
 
                         <div class="grid md:grid-cols-2 md:gap-6">
-                            <div class="mb-6 w-full">
-                                <label for="alamat"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                    No Telp
+
+                            <!-- No Telepon Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="nomor_telepon" id="nomor_telepon"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
+                                <label for="nomor_telepon"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    No Telepon
                                 </label>
-                                <input type="text" id="alamat"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="" autocomplete="off">
                             </div>
 
-                            <div class="mb-6 w-full">
-                                <label for="alamat"
-                                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                            <!-- Email Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="email" name="email_organisasi" id="email_organisasi"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
+                                <label for="email_organisasi"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Email
+                                </label>
+                            </div>
+                        </div>
+
+                        <div class="grid md:grid-cols-2 md:gap-6">
+
+                            <!-- Kecamatan Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="kecamatan" id="kecamatan"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
+                                <label for="kecamatan"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Kecamatan
+                                </label>
+                            </div>
+
+                            <!-- Kabupaten Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="kabupaten" id="kabupaten"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
+                                <label for="kabupaten"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                     Kabupaten
                                 </label>
-                                <input type="text" id="alamat"
-                                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                    placeholder="" autocomplete="off">
                             </div>
                         </div>
 
-                        <div class="mb-6 w-full">
-                            <label for="alamat" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                                Provinsi
-                            </label>
-                            <input type="text" id="alamat"
-                                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="" autocomplete="off">
+                        <div class="grid md:grid-cols-2 md:gap-6">
+
+                            <!-- Provinsi Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <input type="text" name="provinsi" id="provinsi"
+                                    class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                                    placeholder=" " autocomplete="off" required />
+                                <label for="provinsi"
+                                    class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+                                    Provinsi
+                                </label>
+                            </div>
+
+                            <!-- Admin Input -->
+                            <div class="relative z-0 w-full mb-6 group">
+                                <!-- <label for="underline_select" class="sr-only">Underline select</label> -->
+
+                            </div>
                         </div>
 
-                        <button type="submit"
-                            class="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><i
-                                class="fa-solid fa-plus"></i></a></button>
+                        <!-- Button -->
+                        <div class="flex justify-between">
+                            <a class="focus:outline-none text-white bg-red-500 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900"
+                                href="javascript:history.go(-1)"> <i class="fa-solid fa-arrow-left"></i></a>
+                            <button type="submit"
+                                class="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800"><i
+                                    class="fa-solid fa-check"></i></button>
+                        </div>
                     </form>
-                </div>
 
+                </div>
             </div>
+
         </div>
     </div>
-    </div>
 </body>
+
+<script>
+function showPassword() {
+    var passwordInput = document.getElementById("password");
+    var showPassCheckbox = document.getElementById("showpass");
+    if (showPassCheckbox.checked) {
+        passwordInput.type = "text";
+    } else {
+        passwordInput.type = "password";
+    }
+}
+</script>
 
 </html>

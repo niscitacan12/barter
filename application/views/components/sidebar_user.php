399df6788d8cd69b2db227d49a8ad5dacaf060fb
sardewi461 <sardewi461@gmail.com>
2023-11-27 11:59:24 +0700

    push

--- application/views/components/sidebar_user.php
+++ application/views/components/sidebar_user.php
@@ -10,6 +10,13 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet" />
 </head>
 
+<?php
+$id_user = $_SESSION['id']; // Misalkan informasi session disimpan dalam $_SESSION
+$email = $_SESSION['email'];
+$username = $_SESSION['username'];
+$image = $_SESSION['image'];
+?>
+
 <body>
 
     <!-- Navbar -->
@@ -44,11 +51,7 @@
                                 class="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
                                 aria-expanded="false" data-dropdown-toggle="dropdown-user">
                                 <span class="sr-only">Open user menu</span>
-                                <!-- <a href="<?php echo base_url(
-                                    'user/profile'
-                                ); ?>"> -->
-                                <img class="w-8 h-8 rounded-full"
-                                    src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                                <img class="w-8 h-8 rounded-full" src="<?= base_url('images/user/' . $image); ?>"
                                     alt="user photo"></a>
                             </button>
                         </div>
@@ -56,10 +59,10 @@
                             id="dropdown-user">
                             <div class="px-4 py-3" role="none">
                                 <p class="text-sm text-gray-900 dark:text-white" role="none">
-                                    Neil Sims
+                                    <?= $username ?>
                                 </p>
                                 <p class="text-sm font-medium text-gray-900 truncate dark:text-gray-300" role="none">
-                                    neil.sims@flowbite.com
+                                    <?= $email ?>
                                 </p>
                             </div>
                             <ul class="py-1" role="none">
@@ -68,16 +71,6 @@
                                         dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
                                         role="menuitem">Profile</a>
                                 </li>
-                                <!-- <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Settings</a>
-                                </li>
-                                <li>
-                                    <a href="#"
-                                        class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"
-                                        role="menuitem">Earnings</a>
-                                </li> -->
                                 <li>
                                     <a onclick="confirmLogout();"
                                         class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white"

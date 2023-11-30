1f601a2656fc679cd8032bd92b47e3d860a6fd8f
azifaa <dwiazifatuljannah@gmail.com>
2023-11-30 16:29:46 +0700

    ButtonKaryawanDanJabatan

--- application/views/page/admin/jabatan/jabatan.php
+++ application/views/page/admin/jabatan/jabatan.php
@@ -60,33 +60,34 @@
                                         <?php echo $no; ?>
                                     </th>
                                     <td class="px-6 py-4">
-                                        <?php echo $row->nama_jabatan; ?>
+                                    <?php echo $row['nama_jabatan']; ?>
                                     </td>
                                     <td class="px-6 py-4">
-                                        <?php echo jumlah_karyawan($row->id_jabatan); ?>
+                                        <?php echo jumlah_karyawan($row['id_jabatan']); ?>
                                     </td>
                                     <td class="px-6 py-4">
-
+                                    <div class="flex justify-content-between">
                                         <a type="button" href="<?= base_url(
-                                            'admin/detail_jabatan/' . $row->id_jabatan
+                                            'admin/detail_jabatan/' . $row['id_jabatan']
                                         ); ?>"
                                             class="text-white bg-indigo-500 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 mx-1 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
                                             <i class="fa-solid fa-circle-info"></i>
                                         </a>
                                         <a type="button" href="<?php echo base_url(
                                             'admin/update_jabatan/' .
-                                                $row->id_jabatan
+                                                $row['id_jabatan']
                                         ); ?>"
                                             class="text-white bg-yellow-400 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
                                             <i class="fa-solid fa-pen-to-square"></i>
                                         </a>
                                         <a type="button" href="<?php echo base_url(
                                             'admin/hapus_jabatan/' .
-                                                $row->id_jabatan
+                                                $row['id_jabatan']
                                         ); ?>"
                                             class="text-white bg-red-600 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
                                             <i class="fa-solid fa-trash"></i>
                                         </a>
+                                    </div>
                                     </td>
                                 </tr>
                                 <?php

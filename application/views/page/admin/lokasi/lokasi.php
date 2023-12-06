e3ba221ca82056dd2f85451e6ecd72f931b4c8ff
azifaa <dwiazifatuljannah@gmail.com>
2023-12-06 11:15:00 +0700

    SweetalertHapusLokasi

--- application/views/page/admin/lokasi/lokasi.php
+++ application/views/page/admin/lokasi/lokasi.php
@@ -97,13 +97,11 @@
                                         class="text-white bg-yellow-400 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
                                         <i class="fa-solid fa-pen-to-square"></i>
                                     </a>
-                                    <a type="button" href="<?php echo base_url(
-                                            'admin/hapus_lokasi/' .
-                                                $data->id_lokasi
-                                        ); ?>"
-                                        class="text-white bg-red-600 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
-                                        <i class="fa-solid fa-trash"></i>
+                                    <a href="javascript:void(0);" onclick="hapusLokasi('<?php echo $data->id_lokasi; ?>')"
+                                    class="text-white bg-red-600 hover:bg-indigo-800 focus:ring-4 focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-indigo-600 dark:hover:bg-indigo-700 focus:outline-none dark:focus:ring-indigo-800">
+                                    <i class="fa-solid fa-trash"></i>
                                     </a>
+
                                 </div>
                             </td>
 

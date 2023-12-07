36308da7afb8eb31ede718ae737aa3ee76963b51
dewipsari15 <sardewi461@gmail.com>
2023-12-07 14:25:04 +0700

    update

--- application/views/page/super_admin/admin/detail_admin.php
+++ application/views/page/super_admin/admin/detail_admin.php
@@ -69,7 +69,8 @@
                         <div class="relative z-0 w-full mb-6 group">
                             <input type="text" name="username" id="username"
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" value="<?php echo $admin->username; ?>" required />
+                                placeholder=" " autocomplete="off" value="<?php echo $admin->username; ?>" required
+                                readonly />
                             <label for="username"
                                 class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
                                 username</label>
@@ -81,7 +82,8 @@
                         <div class="relative z-0 w-full mb-6 group">
                             <input type="text" name="nama_depan" id="nama_depan"
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" value="<?php echo $admin->nama_depan; ?>" required />
+                                placeholder=" " autocomplete="off" value="<?php echo $admin->nama_depan; ?>" required
+                                readonly />
                             <label for="nama_depan"
                                 class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama
                                 Depan</label>
@@ -89,8 +91,8 @@
                         <div class="relative z-0 w-full mb-6 group">
                             <input type="text" name="nama_belakang" id="nama_belakang"
                                 class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                                placeholder=" " autocomplete="off" value="<?php echo $admin->nama_belakang; ?>"
-                                required />
+                                placeholder=" " autocomplete="off" value="<?php echo $admin->nama_belakang; ?>" required
+                                readonly />
                             <label for="nama_belakang"
                                 class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Nama
                                 Belakang</label>

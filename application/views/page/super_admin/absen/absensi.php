764cdf2a6d1849c71eefbf545d5a0c85b749a088
Aveceena07 <mingave11@gmail.com>
2024-01-10 11:17:49 +0700

    pagination

--- application/views/page/super_admin/absen/absensi.php
+++ application/views/page/super_admin/absen/absensi.php
@@ -35,7 +35,7 @@
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="px-6 py-3">
-                                    No
+                                    Noo
                                 </th>
                                 <th scope="col" class="px-6 py-3">
                                     Nama

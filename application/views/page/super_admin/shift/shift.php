42d707a9e6f3fd3a3ae83babcbf11a91fea1c54b
daniii71 <daniaja0171@gmail.com>
2024-01-12 16:42:50 +0700

    superadmin

--- application/views/page/super_admin/shift/shift.php
+++ application/views/page/super_admin/shift/shift.php
@@ -33,7 +33,7 @@
                 <hr>
 
                 <!-- Tabel -->
-                <div class="mt-4">
+                <div class="relative overflow-x-auto mt-4">
                     <table id="shift" class="w-full text-sm text-left text-gray-500 dark:text-gray-400 mb-4">
 
                         <!-- Tabel Head -->
